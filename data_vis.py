ed472a313808ff65b4eaefc11141ae344351c9bc
Ola Martyna <ola@xpilch.com>
2022-03-09 12:22:38 +0000

    Visualisation of PCA should work now

--- data_vis.py
+++ data_vis.py
@@ -86,7 +86,7 @@ def vis():
 
     # Projector Tensorflow
 
-    visualisation = "https://projector.tensorflow.org/?config=https://gist.githubusercontent.com/scutellaria/6dd9a7008fe925a2d4ddaf8498ea79cb/raw/1d10517d146e190b8ca17ca1343c102265f883b1/config.json"
+    visualisation = "https://projector.tensorflow.org/?config=https://gist.githubusercontent.com/scutellaria/6b7665ede566f36088a87b33da2ba620/raw/8d0c5d752a6ceda80da66c12ad0b8589af0eff25/ml_config.json"
     st.write(f"check out this [link]({visualisation})")
     components.iframe(visualisation, scrolling=False, width=1200, height=800)
 
