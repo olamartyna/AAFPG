190b183b2e8d9d1ef6c617d699a3a27fc06541df
Ola Martyna <ola@xpilch.com>
2022-03-10 12:11:17 +0000

    image adj

--- home.py
+++ home.py
@@ -12,10 +12,20 @@ def welcome():
     path_github = "/app/aafpg/"
     # col1= st.columns(1)
 
-    st.image(path_github + 'AAFPG/data/home page image.jpg', width = 1100)
+    # st.image(path_github + 'AAFPG/data/home page image.jpg', width = 1100)
 
+    col1, col2, col3 = st.beta_columns([1,6,1])
+
+    with col1:
+    st.write("")
+
+    with col2:
+    st.image("path_github + 'AAFPG/data/home page image.jpg")
+
+    with col3:
+    st.write("")
 def app():
-    with open('style.css') as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html = True)
+    # with open('style.css') as f:
+    #     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html = True)
 
     welcome()
