16719a32b5b1f1ec241d51420cd4c1a9b56873a9
Ola Martyna <ola@xpilch.com>
2022-03-10 12:08:39 +0000

    image adj

--- home.py
+++ home.py
@@ -8,10 +8,14 @@ def welcome():
     # from PIL import Image
     # image = Image.open('AAFPG/data/home page image.jpg')
     # st.image(image)
+
     path_github = "/app/aafpg/"
     # col1= st.columns(1)
+
     st.image(path_github + 'AAFPG/data/home page image.jpg', width = 1100)
 
-
 def app():
+    with open('style.css') as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html = True)
+
     welcome()
