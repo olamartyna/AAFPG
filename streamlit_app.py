770deba7baf11443eb6ac4beaad82bf318bd7a5f
Ola Martyna <ola@xpilch.com>
2022-03-10 15:21:16 +0000

    wide layout....?

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import home, data_vis, playlist_visualisation
-
+st.set_page_config(layout="wide")
 
 PAGES = {
     "Home page": home,
     "Visualisation": playlist_visualisation,
     "Tensorflow Projector": data_vis
+    "Playlist": playlist
  }
 
 st.sidebar.write('### Menu')
