e57900fde4040c63c1709a73a14e7a99300580c5
Ola Martyna <ola@xpilch.com>
2022-03-10 18:14:09 +0000

    Better layout for Proj Tens - font size

--- data_vis.py
+++ data_vis.py
@@ -3,25 +3,20 @@ import streamlit as st
 
 def vis():
 
-    # st.header('The Sonic Landscape through Tensorflow Projector')
     st.markdown("<h1 style='text-align: center; color: black;'>The Sonic Landscape through Tensorflow Projector</h1>", unsafe_allow_html=True)
 
     # Projector Tensorflow
 
     visualisation_ml = "https://projector.tensorflow.org/?config=https://gist.githubusercontent.com/scutellaria/6b7665ede566f36088a87b33da2ba620/raw/8d0c5d752a6ceda80da66c12ad0b8589af0eff25/ml_config.json"
-    st.write(f"Visualisation of features obtained from Machine Learning models [link]({visualisation_ml})")
-
+    # st.write(f"Visualisation of features obtained from Machine Learning models [link]({visualisation_ml})")
+    st.write(" ")
+    st.write(" ")
     st.subheader(f"Visualisation of features obtained from Machine Learning models [link]({visualisation_ml})")
-    # components.iframe(visualisation_ml, scrolling=False, width=1200, height=800)
 
 
     visualisation_dl = "https://projector.tensorflow.org/?config=https://gist.githubusercontent.com/scutellaria/10af190769620674257419f80d5b50d7/raw/25027fd687c3bc0ac4285c3745cea592cca8eb06/config.json"
-    st.write(f"Visualisation of features obtained from Deep Learning models [link]({visualisation_dl})")
-    # st.subheader(f"Visualisation of features obtained from Deep Learning models [link]({visualisation_dl})")
-    # components.iframe(visualisation_dl, scrolling=False, width=1200, height=800)
-
-# def header(url):
-#      st.markdown(f'<p style="background-color:#0066cc;color:#33ff33;font-size:24px;border-radius:2%;">{url}</p>', unsafe_allow_html=True)
+    #st.write(f"Visualisation of features obtained from Deep Learning models [link]({visualisation_dl})")
+    st.subheader(f"Visualisation of features obtained from Deep Learning models [link]({visualisation_dl})")
 
 
 def app():
